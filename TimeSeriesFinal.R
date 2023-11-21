2c2286b55cf0582035caeea5a573ac39400a4ac2
Dario Greco <43359382+dariogreco21@users.noreply.github.com>
2023-11-21 13:02:34 -0800

    M5 and M1
    lalalalallala

--- TimeSeriesFinal.R
+++ TimeSeriesFinal.R
@@ -10,7 +10,8 @@ tsplot(total)
 
 # total.c <-  window(total, end = 2020)  # Look at Pre-Covid levels 
 
-total.l <- log(total) # log total
+total.l <- log(total)
+tsplot(total.l)# log total
 total.l.d <- diff(total.l, 1) # difference log total 
 
 ## Plotting log ting 
@@ -23,15 +24,16 @@ pacf(total.l.d)
 ### USE Model$ttable to look at significant coefficients 
 # NOTE: Starting from a lower p, q value dont pass Ljung -Box test 
 M1 <- sarima(total.l, 0,1,12) 
-M2 <- sarima(total.l, 12,1,0) 
+M2 <- sarima(total.l, 2,1,11) 
 M3 <- sarima(total.l, 11,1,0)
-M4 <- sarima(total.l, 11,1,1) 
-M5 <- sarima(total.l, 11,1,2)
+M4 <- sarima(total.l, 11,1,3) 
+M5 <- sarima(total.l, 11,1,2) # BEST 
 M6 <- sarima(total.l, 12,1,2)
-M7 <-sarima(total.l, 12, 1, 1) 
+M7 <-sarima(total.l, 12, 1, 1) # Over fit 
+M8 <-sarima(total.l, 12,1,0) 3 # BAD
 
+M9 <-sarima(total.l, 0,1,12, 0,0,2, 12)
 
-# Winner Model 7,6,5, 1
 AICv <- c(M1$AIC, M2$AIC, M3$AIC, M4$AIC, M5$AIC, M6$AIC, M7$AIC)
 BestAIC <- order(AICv)[1:4] # Returns four best models with AIC criterion 
  # NOTE: MODEL 6 and 5 are similar we just look at model 5 for now 
@@ -59,7 +61,7 @@ p7  = LJB_pvalues(M7, 35, 13); p5 = LJB_pvalues(M5, 35, 14); p1 = LJB_pvalues(M1
 # DENOTED WINNING MODELS Mi by Modeli
 
 
-Model1 <-arima(total.l, c(12,1,1))
+Model1 <-arima(total.l, c(0,1,12))
 fore1 <- predict(Model1, n.ahead = 24)
 ts.plot(log(total), xlim = c(1972, 2026), ylab = "Price Change") # IS IT PRICE CHANGE 
 
